eff652e0cde04f1aa7af06402596144b3bb4d1bb
Luca Ford <lford@BA-MAC-123.local>
2022-04-07 20:47:11 -0300

    Update blog as markdown doesnt worked

--- components/layout.js
+++ components/layout.js
@@ -1,35 +1,31 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
-
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
 const name = "Luca Ford";
-
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Blog";
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
+        <meta name="description" content="Blog descripción" />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <main className="flex flex-col justify-center px-8 bg-gray-50 dark:bg-gray-900">
-        {children}
-      </main>
+      <header className={styles.header}>
+        {home && <h1 className={utilStyles.heading2Xl}>{name}</h1>}
+      </header>
+      <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
           <Link href="/">
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,16 @@
 import Head from "next/head";
-import Link from "next/link";
-import Image from "next/image";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
 import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <div className="flex flex-col-reverse sm:flex-row items-start">
-        <div className="flex flex-col pr-8">
-          <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
-            Luca Ford
-          </h1>
-          <h2 className="text-gray-700 dark:text-gray-200 mb-4">
-            Senior software engineer @{" "}
-            <span className="font-semibold">Fintech</span>
-          </h2>
-        </div>
-        <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto justify-end">
-          <Image
-            alt="Luca Ford"
-            height={100}
-            width={100}
-            src={require("../public/images/profile.jpg")}
-            className="rounded-full"
-          />
-        </div>
-      </div>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
