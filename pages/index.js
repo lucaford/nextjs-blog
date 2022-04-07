04d936602364cc211c42080a12781756e3288f25
Luca Ford <lford@BA-MAC-123.local>
2022-04-07 14:09:17 -0300

    update background color and description

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
@@ -8,16 +9,26 @@ import Date from "../components/date";
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
+      <div className="flex flex-col-reverse sm:flex-row items-start">
+        <div className="flex flex-col pr-8">
+          <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
+            Luca Ford
+          </h1>
+          <h2 className="text-gray-700 dark:text-gray-200 mb-4">
+            Senior software engineer @{" "}
+            <span className="font-semibold">Fintech</span>
+          </h2>
+        </div>
+        <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto justify-end">
+          <Image
+            alt="Luca Ford"
+            height={100}
+            width={100}
+            src={require("../public/images/profile.jpg")}
+            className="rounded-full"
+          />
+        </div>
+      </div>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
